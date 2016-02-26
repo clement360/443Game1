b3874e66b7cb0a067e0df9c8b4a2aea29fd9398a
Miguel Clement <miguelfclement@gmail.com>
2016-02-26 15:07:42 -0600

    file dump

---
+++ Assets/Generator.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditorInternal;
+
+public class Generator : MonoBehaviour
+{
+    private bool oneIsStopped;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    oneIsStopped = false;
+	}
+
+    public void SetOneIsStopped(bool moving)
+    {
+        oneIsStopped = moving;
+    }
+
+    public bool GetOneIsStopped()
+    {
+        return oneIsStopped;
+    }
+}
---
+++ Assets/Movement.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] private GameObject generator;
+    [SerializeField] private float speed;
+
+    
+    private Rigidbody rb;
+    private Vector3 normalVelocity;
+    private bool moving;
+
+    //private static bool oneIsStopped;
+    // Use this for initialization
+    private void Start()
+    {
+        //oneIsStopped = false;
+        rb = GetComponent<Rigidbody>();
+        moving = true;
+        normalVelocity = rb.velocity = Vector3.left*speed;
+
+    }
+
+    private void ResumeMoving()
+    {
+        rb.velocity = normalVelocity;
+        moving = true;
+        SetOneIsStopped(false);
+    }
+
+    private void StopMoving()
+    {
+        rb.velocity = Vector3.zero;
+        moving = false;
+        SetOneIsStopped(true);
+    }
+
+    private void SetOneIsStopped(bool isMoving)
+    {
+        generator.GetComponent<Generator>().SetOneIsStopped(isMoving);
+    }
+
+    void ToggleMovement()
+    {
+        if (moving && !generator.GetComponent<Generator>().GetOneIsStopped())
+        {
+            StopMoving();
+        }
+        else if (!moving)
+        {
+            ResumeMoving();
+        }
+    }
+
+    private void DetectMouseClick()
+    {
+        if (Input.GetKeyDown("mouse 0"))
+        {
+            ToggleMovement();
+        }
+
+    }
+
+    void OnMouseOver()
+    {
+        DetectMouseClick();
+    }
+}
