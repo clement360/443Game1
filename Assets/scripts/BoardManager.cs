1b0f3be1b8c17a690349e8516b121088a25ff97a
Miguel Clement <miguelfclement@gmail.com>
2016-02-26 15:52:34 -0600

    added boardscript

---
+++ Assets/scripts/BoardManager.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoardManager : MonoBehaviour {
+
+	public void SetupScene()
+	{
+
+	}
+
+}
--- Assets/scripts/GameManager.cs
+++ Assets/scripts/GameManager.cs
@@ -4,6 +4,8 @@ using System.Collections;
 public class GameManager : MonoBehaviour {
 
 	public static GameManager instance = null;
+	public Generator generator;
+	public BoardManager boardScript;
 
 	// Use this for initialization
 	void Awake () {
@@ -11,6 +13,16 @@ public class GameManager : MonoBehaviour {
 			instance = this;
 		else if (instance != this)
 			Destroy (gameObject);
+
+		generator = GetComponent<Generator> ();
+		boardScript = GetComponent<BoardManager> ();
+		InitGame ();
+	}
+
+	void InitGame ()
+	{
+		boardScript.SetupScene ();
+		generator.generateBlocks ();
 	}
 	
 	// Update is called once per frame
